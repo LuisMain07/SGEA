9b28a7628d0e1f08ce94d6bac470b59aa251e1f9
LuisMain07 <bdmainguez00@gmail.com>
2025-04-23 15:02:27 -0500

    index.blade de Exposiciones terminado

--- app/Models/Exposiciones.php
+++ app/Models/Exposiciones.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Obras extends Model
+class Exposiciones extends Model
 {
     use HasFactory;
-    protected $table = 'tb_obras';
+    protected $table = 'tb_exposiciones';
     protected $primaryKey = 'id';
     public $timestamps = false;
 }
